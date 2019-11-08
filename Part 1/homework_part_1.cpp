78e79d6da25e6c1616356b6b33a1c66935b6ba07
jfijewsk <jfijewsk@asu.edu>
2019-11-08 08:42:31 -0600

    added insertion sort method

--- Part 1/homework_part_1.cpp
+++ Part 1/homework_part_1.cpp
@@ -49,8 +49,18 @@ void printArray(int* array, int length) {
     cout << ("\n");
 }
 
-void insertionSort(int* array, int length)
-{
+void insertionSort(int* array, int length) {
+        for (int i = 1; i < length; i++) {
+        int key = *(array + i);
+        int j = i - 1;
+
+        // Loop for sorting in descending order.
+        while (j >= 0 && *(array + j) < key) {
+            *(array + j + 1) = *(array + j);
+            j = j - 1;
+        }
+        *(array + j + 1) = key;
+    }
 }
 
 int factorial(int num)
